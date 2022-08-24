021f76e58e1e2011a4cd4d093f99d523b3c0c28f
joaoo <joaooliveira1999@outlook.com>
2022-08-23 22:36:43 -0300

    Aula 1

---
+++ aula1.js
@@ -0,0 +1,27 @@
+// video 1
+// objetos introduçao
+
+// const listaCPF = ["1092030921", "902391930"]; // arrays
+
+// const cliente = { // POO
+//   nome: "Joao",
+//   idade: 22,
+//   cpf: "832983912",
+//   email: "joao@email.com",
+// };
+
+// video 2
+// printar infos
+
+const cliente = {
+  nome: "Joao",
+  idade: 22,
+  cpf: "832983912",
+  email: "joao@email.com",
+};
+
+console.log(
+  `O nome do cliente é: ${cliente.nome} e tem ${cliente.idade} anos.`
+);
+
+console.log(cliente.cpf.substring(0, 3));
