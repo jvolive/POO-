c91cdcc635372ff8b0a148d8356904151e5b264a
joaoo <joaooliveira1999@outlook.com>
2022-08-25 22:25:25 -0300

    Aula de objetos

--- aula1.js
+++ aula1.js
@@ -13,15 +13,47 @@
 // video 2
 // printar infos
 
-const cliente = {
-  nome: "Joao",
-  idade: 22,
-  cpf: "832983912",
-  email: "joao@email.com",
-};
+// const cliente = {
+//   nome: "Joao",
+//   idade: 22,
+//   cpf: "832983912",
+//   email: "joao@email.com",
+// };
 
-console.log(
-  `O nome do cliente é: ${cliente.nome} e tem ${cliente.idade} anos.`
-);
+// console.log(
+//   `O nome do cliente é: ${cliente.nome} e tem ${cliente.idade} anos.`
+// );
 
-console.log(cliente.cpf.substring(0, 3));
+// console.log(cliente.cpf.substring(0, 3));
+
+// video 3
+// acessar lista
+
+// const cliente = {
+//   nome: "Joao",
+//   idade: 22,
+//   cpf: "832983912",
+//   email: "joao@email.com",
+// };
+
+// // const chaves = ["nome", "idade", "cpf", "email"]; // array
+
+// // // console.log(cliente[chaves[0]]); // acessar um dado da array
+
+// // chaves.forEach((info) => console.log(cliente[info])); // acessar o array inteira
+
+// video 4
+// adicionar campos
+
+// const cliente = {
+//   nome: "Joao",
+//   idade: 22,
+//   cpf: "832983912",
+//   email: "joao@email.com",
+// };
+
+// console.log(cliente);
+
+// cliente.fone = "8219389218";
+
+// console.log(cliente);
---
+++ aula2.js
@@ -0,0 +1,100 @@
+//video 1
+// acessar telefones
+
+// const cliente = {
+//   nome: "Joao",
+//   idade: 22,
+//   cpf: "832983912",
+//   email: "joao@email.com",
+//   fones: ["55982938", "3281931092"],
+// };
+
+// cliente.fones.forEach((fone) => console.log(fone));
+
+// video2
+// objeto dentro objeto
+
+// const cliente = {
+//   nome: "Joao",
+//   idade: 22,
+//   cpf: "832983912",
+//   email: "joao@email.com",
+//   fones: ["55982938", "3281931092"],
+// };
+
+// // colocando dados como objetos
+// cliente.dependentes = {
+//   nome: "fulano",
+//   parentesco: "pai",
+//   dataNasc: "25/12/2030",
+// };
+
+//console.log(cliente);
+
+//alterando dados nos objetos
+//cliente.dependentes.nome = "Fulano Malfoy";
+
+//console.log(cliente);
+
+//video 3
+// agregando infomacao em dependentes
+
+// const cliente = {
+//   nome: "Joao",
+//   idade: 22,
+//   cpf: "832983912",
+//   email: "joao@email.com",
+//   fones: ["55982938", "3281931092"],
+//   dependentes: [
+//     {
+//       nome: "fulano",
+//       parentesco: "pai",
+//       dataNasc: "25/12/2030",
+//     },
+//   ],
+// };
+
+// cliente.dependentes.push({ // o push add
+//   nome: "Draco",
+//   parentesco: "filho",
+//   dataNasc: "16/03/1999",
+// });
+
+// console.log(cliente);
+
+// const maisNovo = cliente.dependentes.filter( // aqui ele ta filtrando
+//   (dependente) => dependente.dataNasc <= "1980"
+// );
+
+// console.log(maisNovo[0].nome, maisNovo[0].dataNasc); // aqui ele t aselecionando o mais novo. O [0] é pq o filter retorna como array, dai no caso ele so vai acessar o primeiro indice
+
+//video 4
+// fazendo depositos
+
+const cliente = {
+  nome: "Joao",
+  idade: 22,
+  cpf: "832983912",
+  email: "joao@email.com",
+  fones: ["55982938", "3281931092"],
+  dependentes: [
+    {
+      nome: "fulano",
+      parentesco: "pai",
+      dataNasc: "25/12/2030",
+    },
+    {
+      nome: "Draco",
+      parentesco: "filho",
+      dataNasc: "16/03/1999",
+    },
+  ],
+  saldo: 100,
+  depositar: function (valor) {
+    this.saldo += valor; // this este objeto
+  },
+};
+
+console.log(cliente.saldo);
+cliente.depositar(100);
+console.log(cliente.saldo);
